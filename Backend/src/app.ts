da858dcb161cbe7d413c6303cc2ccb5e07a320c8
Udbhav <udbhavkumar54@gmail.com>
2026-01-17 15:10:56 +0530

    user routes and controllers

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
-import morgan from "morgan"
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 
 config()
 
@@ -11,6 +12,8 @@ app.use(express.json())
 
 // remove it in production 
 app.use(morgan("dev"))
+
+app.use("api/v1", appRouter)
 
 
 export default app;
